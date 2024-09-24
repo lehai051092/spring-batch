e9169e3178b4d6d628572a9b5624f04b191399a2
Le Hai <le.thanh.hai@sun-asterisk.com>
2024-09-24 14:20:40 +0700

    Testing job

--- src/test/java/example/springbatch/SpringBatchApplicationTests.java
+++ src/test/java/example/springbatch/SpringBatchApplicationTests.java
@@ -1,35 +1,46 @@
 package example.springbatch;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.springframework.batch.core.*;
-import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
+@SpringBatchTest
 @SpringBootTest
 @ExtendWith(OutputCaptureExtension.class)
 class SpringBatchApplicationTests {
 
     @Autowired
-    private Job job;
+    private JobLauncherTestUtils jobLauncherTestUtils;
 
     @Autowired
-    private JobLauncher jobLauncher;
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @BeforeEach
+    public void setUp() {
+        this.jobRepositoryTestUtils.removeJobExecutions();
+    }
 
     @Test
     void testJobExecution(CapturedOutput output) throws Exception {
         // given
-        JobParameters jobParameters = new JobParametersBuilder()
+        JobParameters jobParameters = this.jobLauncherTestUtils.getUniqueJobParametersBuilder()
                 .addString("input.file", "/some/input/file")
-                .addString("file.format", "csv", false)
                 .toJobParameters();
+
         // when
-        JobExecution jobExecution = this.jobLauncher.run(this.job, jobParameters);
+        // ** Update the following line:
+        JobExecution jobExecution = this.jobLauncherTestUtils.launchJob(jobParameters);
+
         // then
         Assertions.assertTrue(output.getOut().contains("processing billing information from file /some/input/file"));
         Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
